104c23a7a0eb14c68c01962e6428da68bea3d776
Raj <raju.11811924@lpu.in>
2021-10-31 23:17:30 +0530

    Removed faulty code lines

--- quickStart.sh
+++ quickStart.sh
@@ -8,18 +8,3 @@ read -p "Package name : " package
 mvn archetype:generate -DgroupId=$package -DartifactId=$project  -Darchetypeproject=maven-archetype-quickstart -DinteractiveMode=booleanValue
 
 echo "$project successfuly created, best of luck for your endeavours!"
-
-# read -p "Do you want to integrate eclipse in $project? (y/n) : " isEclipseProject
-
-# if[$isEclipseProject == 'y']
-# then 
-#     echo "Intializing  Eclipse integration in $project"
-
-#     mvn eclipse:eclipse
-    
-#     echo "Eclipse integrations Successful! in $project"
-#     echo "Make something special"
-# else
-#     echo "Alright, make something special"
-
-# exit
